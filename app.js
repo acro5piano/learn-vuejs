209196f66860e8cc8d400280dde0cffea6d5cd58
gosho-kazuya <gosho-kazuya@919.jp>
2016-12-29 00:50:46 +0000

    Complete client-side todo app

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    tasks: [
+      { text: 'Learn JavaScript', editable: false},
+      { text: 'Learn Vue' ,  editable: false},
+      { text: 'Build todo list', editable: false},
+    ],
+    newTaskName: '',
+  },
+  methods: {
+    addTask: function () {
+      this.tasks.push({ text: this.newTaskName})
+      this.newTaskName = ''
+    },
+    deleteTask: function (index) {
+      this.tasks.splice(index, 1)
+    },
+    editTask: function (index, text) {
+      this.tasks[index].text = text
+      this.tasks[index].editable = false
+    },
+    makeTaskEditable: function (index) {
+      this.tasks[index].editable = true
+    },
   }
+})
+
+var head = new Vue({
+  el: '#head',
+  data: {
+    title: 'Todo list'
+  },
 })
